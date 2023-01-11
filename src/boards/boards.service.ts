f2c95c95a372ce7798761029d4eeb457a00637d1
c0dject <creseeds@gmail.com>
2023-01-12 04:14:36 +0900

    NotFoundException 적용

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -29,7 +29,15 @@ export class BoardsService {
   }
 
   getBoardbyId(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+
+    if (!found) {
+      // throw new NotFoundException(); // 기본 값
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+      // 에러메세지 삽입시
+    }
+
+    return found;
   }
 
   deleteBoard(id: string): void {
