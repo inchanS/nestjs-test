8fd88ea6fe73be8e4081c8477878cb4379d1ca6c
c0dject <creseeds@gmail.com>
2023-01-13 10:46:06 +0900

    auth 회원가입 API 구현

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,14 @@
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CustomRepository } from '../db/typeorm-ex.decorator';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    await this.save(user);
+  }
+}
