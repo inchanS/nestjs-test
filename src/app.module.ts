07a5915d6421904455545d980aad328534c343ff
c0dject <creseeds@gmail.com>
2023-01-17 02:06:16 +0900

    .env 및 config 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
 
+const dbConfig = config.get('db');
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '9131',
-  database: 'nestjs_test',
+  type: dbConfig.type,
+  host: process.env.TYPEORM_HOST || dbConfig.host,
+  port: process.env.TYPEORM_PORT || dbConfig.port,
+  username: process.env.TYPEORM_USERNAME || dbConfig.username,
+  password: process.env.TYPEORM_PASSWORD || dbConfig.password,
+  database: process.env.TYPEORM_DATABASE || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true,
+  synchronize: dbConfig.synchronize,
 };
